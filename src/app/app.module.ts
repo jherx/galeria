669467a7a3e2091b5d6ff2ebd255a96947e1736e
Gerson Santos <jhxn07@gmail.com>
2022-02-10 20:56:02 -0600

    creacion del componente galerias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { GaleryComponent } from './pages/galery/galery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
